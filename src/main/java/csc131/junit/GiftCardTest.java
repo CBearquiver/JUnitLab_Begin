9fd50a23a6b0284412624e6cbc1cdf09ea5fad9e
Colton Bearquiver <cbearquiver@csus.edu>
2020-11-24 15:30:20 -0800

    I created a test class for GiftCard.

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,63 @@
+package csc131.junit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class GiftCardTest
+{
+	@Test
+	public void getIssuingStore()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()",
+				 issuingStore, card.getIssuingStore());
+	}
+
+	@Test
+	public void getBalance()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getBalance()",
+				balance, card.getBalance(), 0.001 );
+	}
+
+	@Test
+	public void deduct()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		double deductAmount;
+		String deductTest;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+
+		deductAmount = 27.00;
+		balance = balance - deductAmount;
+		
+		deductTest = "Remaining Balance: " + 
+				String.format("%6.2f", Math.abs(balance));
+		
+		assertEquals("deduct()",
+				deductTest, card.deduct(27));
+	}
+
+}
